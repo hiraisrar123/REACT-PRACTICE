079cb33088ee02b4d44440ced60f91804d754791
hiraisrar123 <hiraisrar47@gmail.com>
2026-02-23 22:14:12 +0500

    123455

--- techzeen/src/Components/FormHandling.jsx
+++ techzeen/src/Components/FormHandling.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const FormHandling = () => {
+    const [name, setName] = useState("");
+    const [option, setOption] = useState("");
+    const [agree, setAgree] = useState(false);
     function getFormData (e){
         e.preventDefault()
-        const formData = new FormData(e.target)
-        for (let [key, value] of formData.entries()) {
-            console.log(key, value)
+            console.log(name, option, agree)
         }
-    }
+    
   return (
     <div>
         <h1>Form Handling In React JS</h1>
         <form onSubmit={getFormData}>
-            <input type="text" placeholder="Enter your name" />
+            <input type="text" placeholder="Enter your name" onChange={(e) => setName (e.target.value)}/>
             <br /><br />
-            <select>
+            <select onChange={(e) => setOption(e.target.value)}>
                 <option>Select an option</option>
                 <option>Option 1</option>
                 <option>Option 2</option>
                 <option>Option 3</option>
             </select>
             <br /><br />
-            <input type="checkbox" id="terms" />
+            <input type="checkbox" id="terms" onChange={(e) => setAgree(e.target.checked)}/>
+            <label htmlFor="terms">I agree to the terms and conditions</label>
             <br /> <br />
             <button type='submit'>Submit</button>
         </form>
